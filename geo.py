726f888ea9fe47c80ae1bf925ced0194ddb8dff8
MAXFrht <58431792+MAXFrht@users.noreply.github.com>
2020-04-30 17:39:26 +0300

    Add files via upload

--- geo.py
+++ geo.py
@@ -5,6 +5,7 @@ import requests
 def get_coordinates(city_name):
     try:
         # url, по которому доступно API Яндекс.Карт
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
         url = "https://geocode-maps.yandex.ru/1.x/"
         # параметры запроса
         params = {
@@ -37,14 +38,15 @@ def get_country(city_name):
     try:
         url = "https://geocode-maps.yandex.ru/1.x/"
         params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
             'geocode': city_name,
             'format': 'json'
         }
         data = requests.get(url, params).json()
         # все отличие тут, мы получаем имя страны
-        return data['response']['GeoObjectCollection']['featureMember'][0][
-            'GeoObject']['metaDataProperty']['GeocoderMetaData'][
-            'AddressDetails']['Country']['CountryName']
+        return data['response']['GeoObjectCollection'][
+            'featureMember'][0]['GeoObject']['metaDataProperty'][
+            'GeocoderMetaData']['AddressDetails']['Country']['CountryName']
     except Exception as e:
         return e
 
